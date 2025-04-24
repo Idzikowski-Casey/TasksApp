d8c936d4c2ed3e1f16b7d6a39947861e9574a9d1
Casey Idzikowski <idzikoj1234@gmail.com>
2025-04-23 21:40:53 -0400

    add presenter key

--- molecule-presenter/api/src/main/java/com/molecule_presenter/api/PresenterFactory.kt
+++ molecule-presenter/api/src/main/java/com/molecule_presenter/api/PresenterFactory.kt
@@ -1,6 +1,8 @@
 package com.molecule_presenter.api
 
 import com.molecule_presenter.api.data.BaseModel
+import dagger.MapKey
+import kotlin.reflect.KClass
 
 /**
  * A factory which will Lazily create presenters when needed. Will make it
@@ -9,3 +11,7 @@ import com.molecule_presenter.api.data.BaseModel
 interface PresenterFactory {
     fun <T : Presenter<out BaseModel>> createPresenter(model: BaseModel): T?
 }
+
+@MapKey
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PresenterKey(val value: KClass<out BaseModel>)
